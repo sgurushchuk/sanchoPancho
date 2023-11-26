b1951e4b20a8e4f47c5bb7e4e854c92c876ea814
gurushchuk <hanzlibnik@gmail.com>
2023-11-26 22:54:10 +0100

    updated verify token

--- backend/helpers/token.js
+++ backend/helpers/token.js
@@ -7,17 +7,6 @@ module.exports = {
 		return jwt.sign(data, sign, { expiresIn: '30d' });
 	},
 	verify(token) {
-		return jwt.verify(token, sign, (err, decoded) => {
-			if (err) {
-				if (err.name === 'TokenExpiredError') {
-					return res.status(401).json({ message: 'Token expired' });
-				} else if (err.name === 'JsonWebTokenError') {
-					return res.status(401).json({ message: 'Invalid token' });
-				} else {
-					return res.status(500).json({ message: 'Internal server error' });
-				}
-			}
-			console.log(decoded);
-		});
+		return jwt.verify(token, sign);
 	},
 };
